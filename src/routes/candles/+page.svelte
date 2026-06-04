5dc08f8605f3d5eac6557bee1bb4589382c75f21
maciejors <maciej.orslowski@gmail.com>
2026-06-04 15:56:43 +0200

    base scaffold for the product list

--- src/routes/candles/+page.svelte
+++ src/routes/candles/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import { createQuery } from '@tanstack/svelte-query';
 	import { fetchAllProducts } from '$lib/supabase/db/products';
 	import Spinner from '$lib/components/Spinner.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import ErrorContainer from '$lib/components/ErrorContainer.svelte';
+	import ProductListItem from './ProductListItem.svelte';
 
 	const query = createQuery(() => ({
 		queryKey: ['products'],
@@ -13,17 +15,19 @@
 
 <main>
 	{#if query.isLoading}
-		<div class="flex w-full items-center justify-center p-6">
+		<div class="flex w-full items-center justify-center p-4">
 			<Spinner />
 		</div>
 	{:else if query.isError}
 		<ErrorContainer error={query.error} />
 	{:else if query.isSuccess}
-		<ul>
-			{#each query.data as product (product.id)}
-				<li>{product.name}</li>
-			{/each}
-		</ul>
-		<Footer />
+		<div transition:fade={{ duration: 400 }}>
+			<ul>
+				{#each query.data as product (product.id)}
+					<ProductListItem {product} />
+				{/each}
+			</ul>
+			<Footer />
+		</div>
 	{/if}
 </main>
---
+++ src/routes/candles/ProductListItem.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { Product } from '$lib/models';
+
+	let { product }: { product: Product } = $props();
+</script>
+
+<li>{product.name}</li>
