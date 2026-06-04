fb8969def088ce15bc802f6e4d06b0dbaaac9524
maciejors <maciej.orslowski@gmail.com>
2026-06-04 15:07:37 +0200

    style the nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,17 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<div class="root-container">
-	<nav>
+<div class="bg-classic-gradient min-h-screen font-forum">
+	<nav class="bg-classic-gradient sticky top-0 mb-3 flex w-full justify-around p-2 shadow-md">
 		<a href={resolve('/about')}>O świecach</a>
-		<a href={resolve('/candles')}>Cennik</a>
+		<a href={resolve('/candles')}>Stwórz zamówienie</a>
 	</nav>
 	{@render children()}
+	<footer class="mt-2 w-full p-2 text-center text-gray-400">2026 Candles Story</footer>
 </div>
+
+<style>
+	nav a {
+		@apply flex-1 text-center hover:underline;
+	}
+</style>
